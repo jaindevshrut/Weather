9e3044099a864ec69fa7596a6cca38e7a82b4ebc
Dev Shrut Jain <138303636+jaindevshrut@users.noreply.github.com>
2024-07-07 16:40:56 +0530

    Update forcast.js
    API from http to https

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -2,7 +2,7 @@ const key = 'QIdZnSLXEdi5EKXAARdAk3BGHcg2G0QF';
 
 //get weather
 const getWeather = async(id)=>{
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base+query);
@@ -13,7 +13,7 @@ const getWeather = async(id)=>{
 
 //get city
 const getCity = async(city) =>{
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
